70d89631d36c22953ee38083646d3a945d9a2949
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-18 09:52:40 +0500

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,36 @@
+let m1 = [
+  [3, 4, 6],
+  [5, 4, 6],
+  [5, 3, 2],
+];
+
+let m4 = 0;
+
+m1.map((eachRow, i) => {
+  eachRow.map((eachRowItem) => {
+    // console.log("eachRowItem: ", eachRowItem);
+    m4 = m4 + eachRowItem;
+  });
+});
+
+// assigmernt: make a program to add two 3x3 matrix
+let m2 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let m3 = 0;
+
+m2.map((eachRow, i) => {
+  eachRow.map((eachRowItem) => {
+    // console.log("eachRowItem: ", eachRowItem);
+    m3 = m3 + eachRowItem;
+  });
+});
+
+console.log("m3", m3);
+console.log("m4", m4);
+
+let m5 = m3 + m4;
+console.log("m5", m5);
